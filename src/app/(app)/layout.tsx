9012431954183759e250de395d0490fbcea5f3e0
Sanjay Sharma <sanjay041024@gmail.com>
2025-08-18 02:25:39 +0000

    everything should be in middle

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -26,8 +26,10 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
       </Sidebar>
       <SidebarInset className="flex flex-col bg-background">
         <Header />
-        <main className="flex-1 overflow-y-auto p-4 md:p-6 lg:p-8">
-          {children}
+        <main className="flex-1 overflow-y-auto p-4 md:p-6 lg:p-8 flex items-center justify-center">
+          <div className="w-full">
+            {children}
+          </div>
         </main>
       </SidebarInset>
     </SidebarProvider>
